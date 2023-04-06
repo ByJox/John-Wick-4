4daa9abd479395206363c0951b1b731e5f3a3acb
Josue Romay <joxromaylp@gmail.com>
2023-04-05 20:03:53 -0400

    Primera Parte

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ActorCard } from './components/ActorCard'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <ActorCard/>
+    </div>
+  )
+}
+
+export default App
